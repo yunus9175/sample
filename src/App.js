baa452719c62b319336aef37b00812e7ce562388
yusuf3919 <yusuf3919@gmail.com>
2021-08-28 23:52:25 +0530

    add new paragraph

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ function App() {
   return (
     <div className="App">
       <h2>Hello React js.....</h2>
-      <p>
-        this is my first paragraph in this project for testing purpose
-        only...... hello all of you
-      </p>
+      
     </div>
   );
 }
