0005ed4aee537a534e697ab4774f9ebc10649878
yunus9175 <yunus626@gmail.com>
2021-08-28 11:42:01 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <h2>Hello React js.....</h2>
-      
+    <div style={{ padding: 5 }}>
+      <Navbar />
+      <div className="App">
+        <h2>Hello React js.....</h2>
+      </div>
     </div>
   );
 }
