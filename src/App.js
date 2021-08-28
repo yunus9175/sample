296eaa2258e7b587d65275caf073eff9fa1e4e60
yunus9175 <yunus626@gmail.com>
2021-08-28 11:22:50 +0530

    change in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello React js.....</h2>
-      <p>this is my first paragraph in this project for testing purpose only......</p>
+      <p>
+        this is my first paragraph in this project for testing purpose
+        only...... hello all of you
+      </p>
     </div>
   );
 }
