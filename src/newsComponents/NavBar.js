07d0123eeeeb4e3768a6e167465399a9e22f6353
yunus9175 <yunus626@gmail.com>
2021-09-05 12:48:21 +0530

    changes in nav bar

--- src/newsComponents/NavBar.js
+++ src/newsComponents/NavBar.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-export class NavBar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              India News
-            </a>
-            <Link className="nav-link" to="/example">
-              Go to Next Project
-            </Link>
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-danger">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            India News
+          </a>
+          <Link className="nav-link" to="/example">
+            Go to Next Project
+          </Link>
 
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                {/* <li className="nav-item">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {/* <li className="nav-item">
                   <a className="nav-link " aria-current="page" href="#">
                     Home
                   </a>
@@ -39,52 +35,47 @@ export class NavBar extends Component {
                     About
                   </a>
                 </li> */}
-                <li>
-                  <Link
-                    className="nav-link"
-                    to="/business"
-                    className="nav-link"
-                  >
-                    business
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/entertainment">
-                    entertainment
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/">
-                    general
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/health">
-                    health
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/science">
-                    science
-                  </Link>
-                </li>{' '}
-                <li>
-                  <Link className="nav-link" to="/sports">
-                    sports
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/technology">
-                    technology
-                  </Link>
-                </li>{' '}
-              </ul>
-            </div>
+              <li>
+                <Link className="nav-link" to="/business" className="nav-link">
+                  business
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/entertainment">
+                  entertainment
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/">
+                  general
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/health">
+                  health
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/science">
+                  science
+                </Link>
+              </li>{' '}
+              <li>
+                <Link className="nav-link" to="/sports">
+                  sports
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/technology">
+                  technology
+                </Link>
+              </li>{' '}
+            </ul>
           </div>
-        </nav>
-      </div>
-    );
-  }
-}
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
